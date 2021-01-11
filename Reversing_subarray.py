fe48ac953dc4b41f4edb549a3a84b35b2856513c
gowthamambati <76831361+gowthamambati@users.noreply.github.com>
2021-01-11 14:22:51 +0530

    Reversing_subarray is done successfully

--- Reversing_subarray.py
+++ Reversing_subarray.py
@@ -1,9 +1,24 @@
-arr=[1,2,3,4,5]
-print(arr)
-k = 3
-if k > len(arr):
-    print(f"{k} value is not valid")
+import random as rn
+n=int(input("Enter no of elements: "))
+ar = rn.sample(range(1,10),n)
+print("input array: ",ar)
+k =int(input("Enter no of elements to be reversed: "))
+print("\n")
+#reverse(ar, n, k) 
+op=[]
+i=0
+if k> len(ar):
+    print("Please enter the number ie less than length of the array")
+    
 else:
-    for i in range(k // 2):
-        arr[i], arr[k - i - 1] = arr[k - i - 1], arr[i]
-print(arr)
+    while(i<n):
+        a =i
+        b=min(i + k - 1, n - 1)
+        while (a < b):
+            ar[a], ar[b] = ar[b], ar[a] 
+            a+= 1; 
+            b-=1
+        i+= k
+for j in range(0, n): 
+    op.append(ar[j])
+print("output array: ",op)
